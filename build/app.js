5d16a06128fd7ff6957404a4278d7cd6590cd22d
atheo810 <atheos810@gmail.com>
2023-04-14 16:43:54 +0800

    menambahkan prisma dan table
    melakukan setting env serta beberapa pengaturan typescript

---
+++ build/app.js
@@ -0,0 +1,33 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getLocationsWithTimezones = void 0;
+const getLocationsWithTimezones = (request, response, next) => {
+    let locations = [
+        {
+            location: "Germany",
+            timezoneName: "Central European Time",
+            timezoneAbbr: "CET",
+            utcOffset: 1,
+        },
+        {
+            location: "China",
+            timezoneName: "China Standard Time",
+            timezoneAbbr: "CST",
+            utcOffset: 8,
+        },
+        {
+            location: "Argentina",
+            timezoneName: "Argentina Time",
+            timezoneAbbr: "ART",
+            utcOffset: -3,
+        },
+        {
+            location: "Japan",
+            timezoneName: "Japan Standard Time",
+            timezoneAbbr: "JST",
+            utcOffset: 9,
+        },
+    ];
+    response.status(200).json(locations);
+};
+exports.getLocationsWithTimezones = getLocationsWithTimezones;
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { log } from "console";
 import express from "express";
+import { getLocationsWithTimezones } from "./app";
 function main() {
   const app = express();
   const port = 5000;
 
   app.get("/", (req, res) => res.send("Express + TypeScript Server"));
+  app.get("/timezones", getLocationsWithTimezones);
   app.listen(port, () => {
     console.log(`[server] server dimulai di http://localhost:${port}`);
   });
 }
+
 main();
